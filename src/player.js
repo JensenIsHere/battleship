bcb58c9ebb87bc9ef891b1a790ebde8e648f1d38
Jensen Ruud <jensen.is.testing@gmail.com>
2022-05-12 19:17:53 -0500

    Computer player makes random moves

--- src/player.js
+++ src/player.js
@@ -1,24 +1,31 @@
 const Player = (pName, playerType) => {
   playerName = pName;
   isComp = playerType;
-  moves = [];
+  boardMemory = Array(10).fill(Array(10).fill(0));
 
   const giveName = () => playerName;
 
   const giveType = () => (isComp == true ? "Computer" : "Human");
 
+  const recall = () => boardMemory;
+
   const scan = (board) => {
-    if (isComp == false) return false;
-    moves = board;
+    if (isComp == false || Array.isArray(board) == false) return false;
+    boardMemory = board;
     return true;
   };
 
   const move = () => {
     if (isComp == false) return false;
-    return Array(2).fill(Math.floor(Math.random() * 10));
+    let newMove;
+    do {
+      newMove = Array(2).fill(Math.floor(Math.random() * 10));
+    } while (boardMemory[newMove[0]][newMove[1]] == -1);
+    boardMemory[newMove[0]][newMove[1]] = -1;
+    return newMove;
   };
 
-  return { giveName, giveType, scan, move };
+  return { giveName, giveType, recall, scan, move };
 };
 
 module.exports = { Player: Player };
